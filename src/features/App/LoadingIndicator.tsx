e3efd2ff2c8f6fcfc3176f117a73825374f8251c
mollehxh <mollehxh@gmail.com>
2022-12-21 15:24:31 -0800

    tech: ROL-1 remove forest

---
+++ src/features/App/LoadingIndicator.tsx
@@ -0,0 +1,33 @@
+type LoadingIndicatorProps = {
+  isVisible: boolean;
+};
+
+export const LoadingIndicator = ({ isVisible }: LoadingIndicatorProps) => {
+  if (!isVisible) return null;
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div className='flex justify-center items-center'>
+        <div
+          className='spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full border-amber-700'
+          style={{
+            borderStyle: 'solid',
+            borderBottomColor: 'transparent',
+          }}
+          role='status'
+        >
+          <span className='hidden'>Loading...</span>
+        </div>
+      </div>
+    </div>
+  );
+};

5aa783532f2c0cf275b018a733d510b35d97c6cb
mollehxh <mollehxh@gmail.com>
2022-12-21 19:20:58 -0800

    add styled-components

--- src/features/react-app/loading-indicator.tsx
+++ src/features/react-app/loading-indicator.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 type LoadingIndicatorProps = {
   isVisible: boolean;
 };
@@ -6,28 +8,29 @@ export const LoadingIndicator = ({ isVisible }: LoadingIndicatorProps) => {
   if (!isVisible) return null;
 
   return (
-    <div
-      style={{
-        position: 'fixed',
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <Element>
       <div className='flex justify-center items-center'>
-        <div
+        <Spinner
           className='spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full border-amber-700'
-          style={{
-            borderStyle: 'solid',
-            borderBottomColor: 'transparent',
-          }}
           role='status'
         >
           <span className='hidden'>Loading...</span>
-        </div>
+        </Spinner>
       </div>
-    </div>
+    </Element>
   );
 };
+
+const Element = styled.div({
+  position: 'fixed',
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const Spinner = styled.div({
+  borderStyle: 'solid',
+  borderBottomColor: 'transparent',
+});

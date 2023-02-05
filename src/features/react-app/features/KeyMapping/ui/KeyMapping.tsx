26c6dd97b023750211c0953e217b28ff61471356
mollehxh <mollehxh@gmail.com>
2023-02-04 18:43:52 -0800

    memoized parseKeyMapping in KeyMapping

--- src/features/react-app/features/KeyMapping/ui/KeyMapping.tsx
+++ src/features/react-app/features/KeyMapping/ui/KeyMapping.tsx
@@ -1,10 +1,11 @@
+import { useMemo } from "react";
 import { keymapping } from "~/features/shared";
 import { Sidebar } from "~shared/ui/Sidebar";
 import { Table } from "~shared/ui/Table";
 import { parseKeyMapping } from "../parseKeyMapping";
 
 export const KeyMapping = () => {
-  const parsedKeyMapping = parseKeyMapping(keymapping);
+  const parsedKeyMapping = useMemo(() => parseKeyMapping(keymapping), []);
 
   return (
     <Sidebar placement="right" className="w-96">

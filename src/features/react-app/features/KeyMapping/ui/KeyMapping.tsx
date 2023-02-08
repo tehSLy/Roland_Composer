d936fd8ea0c66b74d89a413b8f3420a47f132100
mollehxh <mollehxh@gmail.com>
2023-02-07 20:30:09 -0800

    remove Table & refactor KeyMapping

--- src/features/react-app/features/KeyMapping/ui/KeyMapping.tsx
+++ src/features/react-app/features/KeyMapping/ui/KeyMapping.tsx
@@ -1,10 +1,12 @@
 import { useMemo } from "react";
 import { Store } from "effector";
-import { keymapping } from "~/features/shared";
-import { Sidebar } from "~shared/ui/Sidebar";
-import { Table } from "~shared/ui/Table";
-import { parseKeyMapping } from "../parseKeyMapping";
 import { useStore } from "effector-react";
+import { keymapping } from "~/features/shared";
+import { Divider } from "~shared/ui/Divider";
+import { Typography } from "~shared/ui/Typography";
+import { Sidebar } from "~shared/ui/Sidebar";
+import { parseKeyMapping } from "../parseKeyMapping";
+import tw from "tailwind-styled-components";
 
 type KeyMappingProps = {
   visible: Store<boolean>;
@@ -12,14 +14,34 @@ type KeyMappingProps = {
 
 export const KeyMapping = ({ visible: $visible }: KeyMappingProps) => {
   const isVisible = useStore($visible);
-
   const parsedKeyMapping = useMemo(() => parseKeyMapping(keymapping), []);
 
   if (!isVisible) return null;
 
   return (
     <Sidebar placement="right" className="w-96">
-      <Table columns={["Action", "Keybinding"]} dataSource={parsedKeyMapping} />
+      <ul>
+        <ListItem>
+          <Typography bold>Action</Typography>
+          <Typography bold>Keybinding</Typography>
+        </ListItem>
+        <Divider />
+        {parsedKeyMapping.map(([action, keybind]) => {
+          return (
+            <ListItem className="odd:bg-neutral-700">
+              <Typography>{action}</Typography>
+              <Typography>{keybind}</Typography>
+            </ListItem>
+          );
+        })}
+      </ul>
     </Sidebar>
   );
 };
+
+const ListItem = tw.li`
+  grid
+  grid-cols-[3fr_2fr]
+  px-4
+  py-1
+`;
